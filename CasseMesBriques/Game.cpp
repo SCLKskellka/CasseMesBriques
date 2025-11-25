ab97f28708e8861ba73f6a76f0b03360aef8ea32
Christopher Barrera <c.barrera@ensi.lan>
2025-11-25 16:19:21 +0100

    feat: Setup Game class

---
+++ CasseMesBriques/Game.cpp
@@ -0,0 +1,46 @@
+﻿//
+// Created by c.barrera on 25/11/2025.
+//
+//
+// Created by c.barrera on 25/11/2025.
+//
+#include <iostream>
+#include<raylib.h>
+#include <vector>
+#include "Game.h"
+
+void Game::Init() {
+    /**
+     *Ouverture de la fenêtre de jeu
+     **/
+    InitWindow(1600,900,"CustomSnake");
+}
+
+void Game::Run() {
+    Init();
+    //Init
+
+
+    //GameLoop
+    while (!WindowShouldClose()) {
+        ClearBackground(RAYWHITE);
+        //GetFrameTime(); // delta time qui évite la variation du temps en fonction de la puissance machine
+
+        //Update
+
+
+        //Render
+
+        BeginDrawing();
+        //DrawUI
+
+            BeginMode2D(_camera);
+            //DrawGameplay
+                //Fonction de rendu
+
+            EndMode2D();
+        EndDrawing();
+    }
+    CloseWindow();//toujours penser à fermer les fenêtre , elle se détruisent pas automatiquement
+
+}
---
+++ CasseMesBriques/Game.h
@@ -0,0 +1,18 @@
+﻿//
+// Created by c.barrera on 25/11/2025.
+//
+
+#pragma once
+#include<raylib.h>
+
+class Game {
+public:
+    Game() = default;
+    ~Game() = default;
+    void Run();
+private:
+    Camera2D _camera = {0};
+    int _speed;
+    int _score = 0;
+    void Init();
+};
